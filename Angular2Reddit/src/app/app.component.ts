9da6dc8295db65e772401fc6332f688ba3ff0eac
TonyXueDeve <tony.xue@avantport.com>
2018-07-03 16:44:17 +0800

    1.9 渲染多篇，增加显示多篇文章，按照分值排序

--- Angular2Reddit/src/app/app.component.ts
+++ Angular2Reddit/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {Article} from './article/article.model';
 
 @Component({
   selector: 'app-root',
@@ -6,9 +7,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  articles: Article[]; // 渲染多篇文章，这里定义了三篇文章
+  constructor() {
+    this.articles = [
+      new Article('Angular 2', 'http://angular.io', 3),
+      new Article('Fullstack', 'http://Fullstack.io', 2),
+      new Article('Angular Hpmepage', 'http://angular.io', 1)];
+  }
+
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
-    console.log(`Adding article title: ${title.value} and link: ${link.value}`)
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    // 将页面中填写的title，link和这里写死的0 push到Article数组中作为一个新的文章出现在页面中
+    title.value = '';
+    link.value = '';
     return false ;
   }
 
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes );
+  }
 }
--- Angular2Reddit/src/app/article/article.component.ts
+++ Angular2Reddit/src/app/article/article.component.ts
@@ -1,4 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {Article} from './article.model';
 
 @Component({
   selector: 'app-article',
@@ -7,25 +8,22 @@ import {Component, OnInit} from '@angular/core';
   host: {class: 'row'}
 })
 export class ArticleComponent implements OnInit {
-  votes: number;
-  title: string;
-  link: string;
-
+  @Input() article: Article; // 这里是将我们在appcomponent定义的文章数组articles，在页面中遍历之后为article注入到这里
   constructor() {
-    this.title = 'Angular 2';
-    this.link = 'http://angular.io';
-    this.votes = 10;
   }
 // 默认情况下， JavaScript会把click事件冒泡到所有父级组件中。
 // 因为click事件被冒泡到了父级，浏览器就会尝试导航到这个空白链接，于是浏览器就重新刷新了。
 // 解决方案：我们得让click的事件处理器返回false。这能确保浏览器不会尝试刷新页面。
-  voteUp() {
-    this.votes += 1;
+  voteUp(): boolean {
+    this.article.voteUp();
     return false;
   }
+// 这里这样做的原因是之前的做法违反了迪米特法则,也就是说article.component.ts 过多的了解了Article这个类的属性
+// 而迪米特法则是指：一个对象对其他对象的结构或属性所作的假设应该越少越好，所以这里将之前的voteup 和 votedown的对Artile的属性修改方法
+// 添加到了Article类中去，而这里只是去调用Article中的实际voteup和votedown方法
 
-  voteDown() {
-    this.votes -= 1;
+  voteDown(): boolean {
+    this.article.voteDown();
     return false;
   }
   ngOnInit() {
